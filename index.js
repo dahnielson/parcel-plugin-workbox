347fe1c207247eac483cacd7f345840a877ccb8a
Anders Dahnielson <anders@dahnielson.com>
2018-06-09 12:50:16 +0200

    Adding Parcel logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { writeFileSync } = require('fs')
 const path = require('path')
 const workboxBuild = require('workbox-build')
 const uglifyJS = require('uglify-js')
+const logger = require('parcel-bundler/src/Logger')
 
 module.exports = bundle => {
   const { outDir, minify } = bundle.options
@@ -15,11 +16,15 @@ module.exports = bundle => {
       swDest: path.resolve(outDir, '/sw.js')
     }
 
+    logger.status('🛠️', 'Workbox')
     workboxBuild.generateSWString(options).then(swString => {
+      logger.status('✓', 'Service worker generated')
       if (minify) {
         swString = uglifyJS.minify(swString).code
+        logger.status('✓', 'Service worker minified')
       }
       writeFileSync(options.swDest, swString)
+      logger.status('✓', 'Service worker written : ' + options.wDest)
     }, err => {
       logger.err(err)
       throw err
